ba85874ba157b0f3b67bd51c21f88f83913e143a
Felipe Moreira <fmoreira95@hotmail.com>
2021-10-14 21:15:45 -0400

    working on contact form functionality, need to remove invalid class when user begins typing

--- src/components/Contact/Form.jsx
+++ src/components/Contact/Form.jsx
@@ -1,9 +1,74 @@
 import React, { useState, useRef } from "react";
 import "./Form.scss";
 
+const isEmpty = (value) => value.trim() === "";
+const isTenChars = (value) => value.trim().length === 10;
+
 const Form = () => {
+  const [formInputValidity, setFormInputValidity] = useState({
+    name: true,
+    email: true,
+    phone: true,
+    message: true,
+  });
+
+  const nameInputRef = useRef();
+  const emailInputRef = useRef();
+  const phoneInputRef = useRef();
+  const messageInputRef = useRef();
+
+  const confirmHandler = (e) => {
+    e.preventDefault();
+
+    const enteredName = nameInputRef.current.value;
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPhone = phoneInputRef.current.value;
+    const enteredMessage = messageInputRef.current.value;
+
+    const enteredNameIsValid = !isEmpty(enteredName);
+    const enteredEmailIsValid = !isEmpty(enteredEmail);
+    const enteredPhoneIsValid = isTenChars(enteredPhone);
+    const enteredMessageIsValid = !isEmpty(enteredMessage);
+
+    setFormInputValidity({
+      name: enteredNameIsValid,
+      email: enteredEmailIsValid,
+      phone: enteredPhoneIsValid,
+      message: enteredMessageIsValid,
+    });
+
+    const formIsValid =
+      enteredNameIsValid &&
+      enteredPhoneIsValid &&
+      enteredEmailIsValid &&
+      enteredMessageIsValid;
+
+    if (!formIsValid) {
+      return;
+    }
+  };
+
+  let nameControlClasses = "control";
+  if (!formInputValidity.name) {
+    nameControlClasses = "invalid";
+  }
+
+  let emailControlClasses = "control";
+  if (!formInputValidity.email) {
+    emailControlClasses = "invalid";
+  }
+
+  let phoneControlClasses = "control";
+  if (!formInputValidity.phone) {
+    phoneControlClasses = "invalid";
+  }
+
+  let messageControlClasses = "control";
+  if (!formInputValidity.message) {
+    messageControlClasses = "invalid";
+  }
   return (
-    <section className="form__container">
+    <section className="form__container" onSubmit={confirmHandler}>
       <h1 className="form__container--title">Contact Us</h1>
       <p className="form__container--p">
         Ready to take it to the next level? Let’s talk about your project or
@@ -13,41 +78,61 @@ const Form = () => {
       </p>
 
       <form className="form__container--form">
-        <div className="form__container--box">
+        <div>
           <label className="form__container--label" htmlFor="name"></label>
           <input
-            className="form__container--input"
+            className={nameControlClasses}
             type="text"
             id="name"
             placeholder="Name"
+            ref={nameInputRef}
           />
+          {!formInputValidity.name && (
+            <p className="invalid-message">Please enter a valid name!</p>
+          )}
         </div>
         <div>
           <label className="form__container--label" htmlFor="email"></label>
           <input
-            className="form__container--input"
+            className={emailControlClasses}
             type="email"
             id="email"
             placeholder="Email Address"
+            ref={emailInputRef}
           />
+          {!formInputValidity.email && (
+            <p className="invalid-message">
+              Please enter a valid email address!
+            </p>
+          )}
         </div>
         <div>
           <label className="form__container--label" htmlFor="phone"></label>
           <input
-            className="form__container--input"
+            className={phoneControlClasses}
             type="text"
             id="phone"
             placeholder="Phone"
+            ref={phoneInputRef}
           />
+          {!formInputValidity.phone && (
+            <p className="invalid-message">
+              Please enter a valid phone number!
+            </p>
+          )}
         </div>
         <div>
           <label className="form__container--label" htmlFor="message"></label>
           <input
-            className="form__container--input"
+            className={messageControlClasses}
             type="text"
             id="message"
             placeholder="Your Message"
+            ref={messageInputRef}
           />
+          {!formInputValidity.message && (
+            <p className="invalid-message">Please enter a valid message!</p>
+          )}
         </div>
 
         <button className="form__container--btn" type="submit">
