c37d39cf569273f9307976817f37c6230d07f713
Felipe Moreira <fmoreira95@hotmail.com>
2021-10-21 21:31:17 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,70 +1,66 @@
-import React, {Suspense} from "react";
+import React, { Suspense } from "react";
 import Header from "./components/Header/Header";
-
-import WebDesign from "./pages/WebDesign";
-import AppDesign from "./pages/AppDesign";
-import GraphicDesign from "./pages/GraphicDesign";
-import LocationsPage from "./pages/LocationsPage";
-import AboutUs from "./pages/AboutUs";
-import ContactPage from "./pages/ContactPage";
-import { Switch, Route, Redirect } from "react-router-dom";
+import { BoxLoading } from "react-loadingg";
+import { Switch, Route } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import ScrollToTop from "./components/Utility/ScrollToTop";
 
 const HomePage = React.lazy(() => import("./pages/HomePage"));
+const WebDesign = React.lazy(() => import("./pages/WebDesign"));
+const AppDesign = React.lazy(() => import("./pages/AppDesign"));
+const GraphicDesign = React.lazy(() => import("./pages/GraphicDesign"));
+const LocationsPage = React.lazy(() => import("./pages/LocationsPage"));
+const AboutUs = React.lazy(() => import("./pages/AboutUs"));
+const ContactPage = React.lazy(() => import("./pages/ContactPage"));
 
 function App() {
   return (
     <div>
-      <Suspense
-        fallback={
-          <div className="centered">
-              Loading..
-          </div>
-        }
-      <Header />
-      <Switch>
-        <Route path="/" exact>
-          <ScrollToTop />
-          <HomePage />
-        </Route>
-        <Route path="/web-design" exact>
-          <ScrollToTop />
+      <Suspense fallback={<BoxLoading />}>
+        <Header />
+        <Switch>
+          <Route path="/" exact>
+            <ScrollToTop />
+            <HomePage />
+          </Route>
+          <Route path="/web-design" exact>
+            <ScrollToTop />
 
-          <WebDesign />
-        </Route>
-        <Route path="/app-design" exact>
-          <ScrollToTop />
+            <WebDesign />
+          </Route>
+          <Route path="/app-design" exact>
+            <ScrollToTop />
 
-          <AppDesign />
-        </Route>
-        <Route path="/graphic-design" exact>
-          <ScrollToTop />
+            <AppDesign />
+          </Route>
+          <Route path="/graphic-design" exact>
+            <ScrollToTop />
 
-          <GraphicDesign />
-        </Route>
-        <Route path="/about" exact>
-          <ScrollToTop />
+            <GraphicDesign />
+          </Route>
+          <Route path="/about" exact>
+            <ScrollToTop />
 
-          <AboutUs />
-        </Route>
-        <Route path="/locations" exact>
-          <ScrollToTop />
+            <AboutUs />
+          </Route>
+          <Route path="/locations" exact>
+            <ScrollToTop />
 
-          <LocationsPage />
-        </Route>
-        <Route path="/contact">
-          <ScrollToTop />
+            <LocationsPage />
+          </Route>
+          <Route path="/contact">
+            <ScrollToTop />
 
-          <ContactPage />
-        </Route>
-        <Route path="*">
-          <ScrollToTop />
+            <ContactPage />
+          </Route>
+          <Route path="*">
+            <ScrollToTop />
 
-          <HomePage />
-        </Route>
-      </Switch>
-      <Footer />
+            <HomePage />
+          </Route>
+        </Switch>
+        <Footer />
+      </Suspense>
     </div>
   );
 }
